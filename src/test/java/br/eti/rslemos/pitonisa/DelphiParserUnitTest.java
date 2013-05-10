cb792e306b26d60eadd30aa3beba9b0751625a6c
R. Lemos <rslemos@gmail.com>
2013-05-10 20:56:24 -0300

    xpath like expressions for test verification

--- src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
+++ src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
@@ -26,68 +26,93 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenSource;
 import org.junit.Test;
-
-import br.eti.rslemos.pitonisa.delphiParser.GoalContext;
 
 public class DelphiParserUnitTest {
 	private static final List<String> ruleNames = Arrays.asList(delphiParser.ruleNames);
 	
 	@Test
 	public void testUnit001() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit001.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit001) ; .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit001.pas"));
+		assertThat(xpath(context, "/goal").toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit001) ; .))")));
 	}
 
 	@Test
 	public void testUnit002() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit002.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit002) ; (interfaceSection INTERFACE) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit002.pas"));
+		assertThat(xpath(context, "/goal/unit").toStringTree(ruleNames), is(equalTo("(unit UNIT (ident Unit002) ; (interfaceSection INTERFACE) .)")));
 	}
 
 	@Test
 	public void testUnit003() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit003.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit003) ; (implementationSection IMPLEMENTATION) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit003.pas"));
+		assertThat(xpath(context, "/goal/unit").toStringTree(ruleNames), is(equalTo("(unit UNIT (ident Unit003) ; (implementationSection IMPLEMENTATION) .)")));
 	}
 
 	@Test
 	public void testUnit004() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit004.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit004) ; (interfaceSection INTERFACE (usesClause USES (identList (ident uses0)) ;)) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit004.pas"));
+		assertThat(xpath(context, "/goal/unit/interfaceSection").toStringTree(ruleNames), is(equalTo("(interfaceSection INTERFACE (usesClause USES (identList (ident uses0)) ;))")));
 	}
 
 	@Test
 	public void testUnit005() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit005.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit005) ; (interfaceSection INTERFACE (usesClause USES (identList (ident uses0) , (ident uses1)) ;)) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit005.pas"));
+		assertThat(xpath(context, "/goal/unit/interfaceSection").toStringTree(ruleNames), is(equalTo("(interfaceSection INTERFACE (usesClause USES (identList (ident uses0) , (ident uses1)) ;))")));
 	}
 
 	@Test
 	public void testUnit006() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit006.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit006) ; (interfaceSection INTERFACE (interfaceDecl (constSection CONST (constDecl (ident const0) = (constExpr (number 10))) ; (constDecl (ident const1) = (constExpr (string 'value'))) ;))) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit006.pas"));
+		assertThat(xpath(context, "/goal/unit/interfaceSection").toStringTree(ruleNames), is(equalTo("(interfaceSection INTERFACE (interfaceDecl (constSection CONST (constDecl (ident const0) = (constExpr (number 10))) ; (constDecl (ident const1) = (constExpr (string 'value'))) ;)))")));
 	}
 
 	@Test
 	public void testUnit007() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit007.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit007) ; (interfaceSection INTERFACE (interfaceDecl (varSection VAR (varDecl (identList (ident var0) , (ident var1)) : (type (simpleType (ordinalType (ordIdent INTEGER)))) = (constExpr (number 10))) ; (varDecl (identList (ident var2)) : (type (typeId (ident string)))) ; (varDecl (identList (ident var3)) : (type (typeId (ident Byte))) ABSOLUTE (ident var2)) ;))) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit007.pas"));
+		assertThat(xpath(context, "/goal/unit/interfaceSection").toStringTree(ruleNames), is(equalTo("(interfaceSection INTERFACE (interfaceDecl (varSection VAR (varDecl (identList (ident var0) , (ident var1)) : (type (simpleType (ordinalType (ordIdent INTEGER)))) = (constExpr (number 10))) ; (varDecl (identList (ident var2)) : (type (typeId (ident string)))) ; (varDecl (identList (ident var3)) : (type (typeId (ident Byte))) ABSOLUTE (ident var2)) ;)))")));
 	}
 
 	@Test
 	public void testUnit008() throws Exception {
-		GoalContext context = parse(getClass().getResourceAsStream("Unit008.pas"));
-		assertThat(context.toStringTree(ruleNames), is(equalTo("(goal (unit UNIT (ident Unit008) ; (interfaceSection INTERFACE (interfaceDecl (exportedHeading (functionHeading FUNCTION (ident function0) (formalParms ( (formalParm (parameter (identList (ident parm0) , (ident parm1)) : STRING)) ; (formalParm (parameter (identList (ident parm2)) : (simpleType (realType CURRENCY)))) ; (formalParm (parameter (identList (ident parm3) , (ident parm4) , (ident parm5)) : (simpleType (ordinalType (ordIdent INTEGER))))) )) : (simpleType (ordinalType (ordIdent BOOLEAN)))) ;)) (interfaceDecl (exportedHeading (procedureHeading PROCEDURE (ident procedure0) (formalParms ( (formalParm (parameter (identList (ident parm0) , (ident parm1)) : (simpleType (ordinalType (ordIdent INTEGER))))) ))) ;))) .))")));
+		ParserRuleContext context = parse(getClass().getResourceAsStream("Unit008.pas"));
+		assertThat(xpath(context, "/goal/unit/interfaceSection").toStringTree(ruleNames), is(equalTo("(interfaceSection INTERFACE (interfaceDecl (exportedHeading (functionHeading FUNCTION (ident function0) (formalParms ( (formalParm (parameter (identList (ident parm0) , (ident parm1)) : STRING)) ; (formalParm (parameter (identList (ident parm2)) : (simpleType (realType CURRENCY)))) ; (formalParm (parameter (identList (ident parm3) , (ident parm4) , (ident parm5)) : (simpleType (ordinalType (ordIdent INTEGER))))) )) : (simpleType (ordinalType (ordIdent BOOLEAN)))) ;)) (interfaceDecl (exportedHeading (procedureHeading PROCEDURE (ident procedure0) (formalParms ( (formalParm (parameter (identList (ident parm0) , (ident parm1)) : (simpleType (ordinalType (ordIdent INTEGER))))) ))) ;)))")));
 	}
 
-	private GoalContext parse(InputStream input) throws Exception {
+	private ParserRuleContext xpath(ParserRuleContext context, String path) {
+		List<String> parts = new ArrayList<String>(Arrays.asList(path.split("/")));
+
+		while (parts.remove(""));
+		
+		if (parts.get(0).equals(ruleNames.get(context.getRuleIndex())))
+			parts.remove(0);
+		
+	outer:
+		for (String part : parts) {
+			for (int i = 0; i < context.getChildCount()	; i++) {
+				ParserRuleContext candidate = context.getChild(ParserRuleContext.class, i);
+				if (part.equals(ruleNames.get(candidate.getRuleIndex()))) {
+					context = candidate;
+					continue outer;
+				}
+			}
+			
+			throw new IllegalArgumentException();
+			
+		}
+		
+		return context;
+	}
+
+	private ParserRuleContext parse(InputStream input) throws Exception {
 		TokenSource lexer = new delphiLexer(new ANTLRInputStream(input));
 		delphiParser parser = new delphiParser(new CommonTokenStream(lexer));
 		return parser.goal();
