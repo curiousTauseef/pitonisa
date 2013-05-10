b4b460ba994b2696e1207cfacd7616ce107c8e8d
R. Lemos <rslemos@gmail.com>
2013-05-09 23:40:17 -0300

    lexer bootstrap

--- src/main/antlr4/br/eti/rslemos/pitonisa/delphi.g4
+++ src/main/antlr4/br/eti/rslemos/pitonisa/delphi.g4
@@ -1,6 +1,27 @@
 grammar delphi;
 
-goal
-	:
+UNIT			: 'UNIT';
+
+
+ID	: ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*
+    ;
+
+WS
+	: ( ' '
+	  | '\t'
+	  | '\r'
+	  | '\n'
+	  ) -> channel(HIDDEN)
 	;
 
+goal
+	: unit
+	;
+
+unit
+	: UNIT ident ';'
+	;
+
+ident
+	: ID
+	;
--- src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
+++ src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
@@ -1,60 +1,20 @@
 package br.eti.rslemos.pitonisa;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonToken;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenFactory;
 import org.antlr.v4.runtime.TokenSource;
 import org.antlr.v4.runtime.TokenStream;
 import org.junit.Test;
-
-import br.eti.rslemos.pitonisa.delphiParser.GoalContext;
 
 public class DelphiParserUnitTest {
 	
 	@Test
 	public void testBootstrapParser() throws Exception {
-		TokenSource source = new TokenSource() {
-
-			@Override
-			public Token nextToken() {
-				return new CommonToken(Token.EOF);
-			}
-
-			@Override
-			public int getLine() {
-				return 0;
-			}
-
-			@Override
-			public int getCharPositionInLine() {
-				return 0;
-			}
-
-			@Override
-			public CharStream getInputStream() {
-				return null;
-			}
-
-			@Override
-			public String getSourceName() {
-				return null;
-			}
-
-			@Override
-			public void setTokenFactory(TokenFactory<?> factory) {
-			}
-
-			@Override
-			public TokenFactory<?> getTokenFactory() {
-				return null;
-			}
-			
-		};
-		TokenStream input = new CommonTokenStream(source);
+		CharStream text = new ANTLRInputStream("UNIT x;");
+		TokenSource lexer = new delphiLexer(text);
+		TokenStream input = new CommonTokenStream(lexer);
 		delphiParser parser = new delphiParser(input);
-		GoalContext context = parser.goal();
-		
+		parser.goal();
 	}
 }
