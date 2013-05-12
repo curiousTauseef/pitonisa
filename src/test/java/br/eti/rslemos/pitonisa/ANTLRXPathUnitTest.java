ec622364979545004a7cab0719aa34abce1cabb5
R. Lemos <rslemos@gmail.com>
2013-05-11 21:11:32 -0300

    xpath like expression walker moved to own class

---
+++ src/test/java/br/eti/rslemos/pitonisa/ANTLRXPathUnitTest.java
@@ -0,0 +1,120 @@
+package br.eti.rslemos.pitonisa;
+
+import static br.eti.rslemos.pitonisa.ANTLRXPath.xpath;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ANTLRXPathUnitTest {
+	private static final List<String> RULESNAMES = Arrays.asList("A", "B", "C");
+	
+	private ParserRuleContext root_A;
+	private ParserRuleContext root_A_B0;
+	private ParserRuleContext root_A_B0_C0;
+	private ParserRuleContext root_A_B0_C1;
+	private ParserRuleContext root_A_B0_C2;
+	private ParserRuleContext root_A_B1;
+	private ParserRuleContext root_A_B1_C0;
+	private ParserRuleContext root_A_B1_C1;
+	private ParserRuleContext root_A_B1_C2;
+	private ParserRuleContext root_A_B2;
+	private ParserRuleContext root_A_B2_C0;
+	private ParserRuleContext root_A_B2_C1;
+	private ParserRuleContext root_A_B2_C2;
+	
+	@Before
+	public void setup() {
+		root_A = buildParserRuleContext("A");
+		root_A_B0 = buildParserRuleContext("B");
+		root_A_B1 = buildParserRuleContext("B");
+		root_A_B2 = buildParserRuleContext("B");
+		root_A.children = Arrays.asList((ParseTree)root_A_B0, root_A_B1, root_A_B2);
+		root_A_B0_C0 = buildParserRuleContext("C");
+		root_A_B0_C1 = buildParserRuleContext("C");
+		root_A_B0_C2 = buildParserRuleContext("C");
+		root_A_B0.children = Arrays.asList((ParseTree)root_A_B0_C0, root_A_B0_C1, root_A_B0_C2);
+		root_A_B1_C0 = buildParserRuleContext("C");
+		root_A_B1_C1 = buildParserRuleContext("C");
+		root_A_B1_C2 = buildParserRuleContext("C");
+		root_A_B1.children = Arrays.asList((ParseTree)root_A_B1_C0, root_A_B1_C1, root_A_B1_C2);
+		root_A_B2_C0 = buildParserRuleContext("C");
+		root_A_B2_C1 = buildParserRuleContext("C");
+		root_A_B2_C2 = buildParserRuleContext("C");
+		root_A_B2.children = Arrays.asList((ParseTree)root_A_B2_C0, root_A_B2_C1, root_A_B2_C2);
+	}
+
+	private static ParserRuleContext buildParserRuleContext(String token) {
+		return buildParserRuleContext(RULESNAMES.indexOf(token));
+	}
+
+	private static ParserRuleContext buildParserRuleContext(final int ruleIndex) {
+		return new ParserRuleContext() { @Override public int getRuleIndex() { return ruleIndex; }  };
+	}
+
+	@Test
+	public void testEmptyPath() throws Exception {
+		assertThat(xpath(root_A, "", RULESNAMES), is(sameInstance(root_A)));
+	}
+	
+	@Test
+	public void testSingleSlashPath() throws Exception {
+		assertThat(xpath(root_A, "/", RULESNAMES), is(sameInstance(root_A)));
+	}
+	
+	@Test
+	public void testSingleRootPath() throws Exception {
+		assertThat(xpath(root_A, "/A", RULESNAMES), is(sameInstance(root_A)));
+	}
+	
+	@Test
+	public void testTwoPartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B", RULESNAMES), is(sameInstance(root_A_B0)));
+	}
+	
+	@Test
+	public void testThreePartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B/C", RULESNAMES), is(sameInstance(root_A_B0_C0)));
+	}
+	
+	@Test
+	public void testIndexedLastPartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B[0]", RULESNAMES), is(sameInstance(root_A_B0)));
+		assertThat(xpath(root_A, "/A/B[1]", RULESNAMES), is(sameInstance(root_A_B1)));
+		assertThat(xpath(root_A, "/A/B[2]", RULESNAMES), is(sameInstance(root_A_B2)));
+	}
+	
+	@Test
+	public void testIndexedLastOfThreePartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B/C[0]", RULESNAMES), is(sameInstance(root_A_B0_C0)));
+		assertThat(xpath(root_A, "/A/B/C[1]", RULESNAMES), is(sameInstance(root_A_B0_C1)));
+		assertThat(xpath(root_A, "/A/B/C[2]", RULESNAMES), is(sameInstance(root_A_B0_C2)));
+	}
+
+	@Test
+	public void testIndexedMiddleOfThreePartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B[0]/C", RULESNAMES), is(sameInstance(root_A_B0_C0)));
+		assertThat(xpath(root_A, "/A/B[1]/C", RULESNAMES), is(sameInstance(root_A_B1_C0)));
+		assertThat(xpath(root_A, "/A/B[2]/C", RULESNAMES), is(sameInstance(root_A_B2_C0)));
+	}
+
+	@Test
+	public void testMultiIndexedOfThreePartPath() throws Exception {
+		assertThat(xpath(root_A, "/A/B[0]/C[0]", RULESNAMES), is(sameInstance(root_A_B0_C0)));
+		assertThat(xpath(root_A, "/A/B[0]/C[1]", RULESNAMES), is(sameInstance(root_A_B0_C1)));
+		assertThat(xpath(root_A, "/A/B[0]/C[2]", RULESNAMES), is(sameInstance(root_A_B0_C2)));
+		assertThat(xpath(root_A, "/A/B[1]/C[0]", RULESNAMES), is(sameInstance(root_A_B1_C0)));
+		assertThat(xpath(root_A, "/A/B[1]/C[1]", RULESNAMES), is(sameInstance(root_A_B1_C1)));
+		assertThat(xpath(root_A, "/A/B[1]/C[2]", RULESNAMES), is(sameInstance(root_A_B1_C2)));
+		assertThat(xpath(root_A, "/A/B[2]/C[0]", RULESNAMES), is(sameInstance(root_A_B2_C0)));
+		assertThat(xpath(root_A, "/A/B[2]/C[1]", RULESNAMES), is(sameInstance(root_A_B2_C1)));
+		assertThat(xpath(root_A, "/A/B[2]/C[2]", RULESNAMES), is(sameInstance(root_A_B2_C2)));
+	}
+}
--- src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
+++ src/test/java/br/eti/rslemos/pitonisa/DelphiParserUnitTest.java
@@ -26,7 +26,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -89,38 +88,7 @@ public class DelphiParserUnitTest {
 	}
 
 	private ParserRuleContext xpath(ParserRuleContext context, String path) {
-		List<String> parts = new ArrayList<String>(Arrays.asList(path.split("/")));
-
-		while (parts.remove(""));
-		
-		if (parts.get(0).equals(ruleNames.get(context.getRuleIndex())))
-			parts.remove(0);
-		
-	outer:
-		for (String part : parts) {
-			int index = 0;
-			
-			if (part.matches("^.*\\[[0-9]+\\]$")) {
-				String indexPart = part.substring(part.lastIndexOf('[') + 1, part.length() - 1);
-				index = Integer.parseInt(indexPart);
-				part = part.substring(0, part.lastIndexOf('['));
-			}
-				
-			for (int i = 0; i < context.getChildCount()	; i++) {
-				ParserRuleContext candidate = context.getChild(ParserRuleContext.class, i);
-				if (part.equals(ruleNames.get(candidate.getRuleIndex()))) {
-					if (index-- == 0) { 
-						context = candidate;
-						continue outer;
-					}
-				}
-			}
-			
-			throw new IllegalArgumentException();
-			
-		}
-		
-		return context;
+		return ANTLRXPath.xpath(context, path, ruleNames);
 	}
 
 	private ParserRuleContext parse(InputStream input) throws Exception {
